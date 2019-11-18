bc5a9954ff6418859b62d562e98f446ecfc296ff
Nguyen Van Thieu <thieu.nguyenvan@gameloft.com>
2019-11-18 13:43:34 +0700

    'menu'

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,47 +6,48 @@ class Navbar extends Component {
         return (
             <nav className='navbar navbar-expand-sm navbar-dark bg-white shadow-sm nav-header'>
                 <div className='container-fluid d-flex py-2 justify-content-between'>
-                    <div className='nav-left d-flex justify-content-start'>
+                    <div className='nav-left d-flex justify-content-start align-items-center'>
                         <Link className='navbar-brand nav-link p-0' to='/' >
                             <img src={'./assets/logo.png'} alt='store' className='navbar-brand logo' />
                         </Link>
 
-                        <form className="form-inline my-2 my-lg-0">
-                            <input className="form-control mr-sm-1 seach-box" type="search" placeholder="Nhập tên sản phẩm" aria-label="Search" />
-                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
-                        </form>
+                        <div className='menu-top ml-auto'>
+                            <ul className='navbar-nav align-items-bottom'>
+                                <li className='nav-item ml-1 menu-item'>
+                                    <Link to='/' className='nav-link menu-item'>
+                                        trang chủ
+                                    </Link>
+                                </li>
+                                <li className='nav-item ml-1'>
+                                    <Link to='/' className='nav-link menu-item'>
+                                        giới thiệu
+                                    </Link>
+                                </li>
+                                <li className='nav-item ml-1'>
+                                    <Link to='/products' className='nav-link menu-item'>
+                                        sản phẩm
+                                    </Link>
+                                </li>
+                                <li className='nav-item ml-1'>
+                                    <Link to='/' className='nav-link menu-item'>
+                                        sứ mệnh
+                                    </Link>
+                                </li>
+                                <li className='nav-item ml-1'>
+                                    <Link to='/' className='nav-link menu-item'>
+                                        liên hệ
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+
                     </div>
                     <div className='nav-right'>
                         <div className="collapse navbar-collapse" id="navbarResponsive">
-                            <div className='menu-top ml-auto pr-2'>
-                                <ul className='navbar-nav align-items-bottom'>
-                                    <li className='nav-item ml-1 menu-item'>
-                                        <Link to='/' className='nav-link menu-item'>
-                                            trang chủ
-                                    </Link>
-                                    </li>
-                                    <li className='nav-item ml-1'>
-                                        <Link to='/' className='nav-link menu-item'>
-                                            giới thiệu
-                                    </Link>
-                                    </li>
-                                    <li className='nav-item ml-1'>
-                                        <Link to='/products' className='nav-link menu-item'>
-                                            sản phẩm
-                                    </Link>
-                                    </li>
-                                    <li className='nav-item ml-1'>
-                                        <Link to='/' className='nav-link menu-item'>
-                                            sứ mệnh
-                                    </Link>
-                                    </li>
-                                    <li className='nav-item ml-1'>
-                                        <Link to='/' className='nav-link menu-item'>
-                                            liên hệ
-                                    </Link>
-                                    </li>
-                                </ul>
-                            </div>
+                            <form className="form-inline my-2 my-lg-0 pr-2">
+                                <input className="form-control mr-sm-1 seach-box" type="search" placeholder="Nhập tên sản phẩm" aria-label="Search" />
+                                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
+                            </form>
                             <Link to='/cart' className='pr-2'>
                                 <button className='shopping-cart'>
                                     <i className="fas fa-shopping-cart" />
